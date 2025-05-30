3354d00870311caf5ade09e13855bb542b823fd7
T.Neetha Srivalli <neethasrivallit@gmail.com>
2025-05-30 17:12:23 +0530

    Week_2_Assignments

---
+++ may_2025_contributions/PY_F_149_T.Neetha Srivalli_submission/Week 2 Assignments.py/1.Strings_Assignment.py/1]Basic_ques.py/8.Count_sample.py
@@ -0,0 +1,5 @@
+a = "Santa"        #Giving a string
+print("Count of 'a':", a.count('a'))   #Prints the no.of occurances of 'a' in the string
+ #'count()' counts the no.of occurances of a character
+ #Output:
+ # Count of 'a':2
