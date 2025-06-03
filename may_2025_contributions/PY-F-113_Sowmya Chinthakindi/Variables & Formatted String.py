6993d5ff8cefe11720f4f658077bcae88bfd0a1d
sowmya130 <117059514+sowmya130@users.noreply.github.com>
2025-06-03 22:27:30 +0530

    Add files via upload

---
+++ may_2025_contributions/PY-F-113_Sowmya Chinthakindi/Variables & Formatted String.py
@@ -0,0 +1,6 @@
+company = "Toyota"
+market_position= "Leading cars sales company"
+revenue = 787348728347.989
+
+print(f"{company} is currently {market_position}")
+print(f"In 2025 this the sales for this year {revenue}")
---
+++ may_2025_contributions/PY-F-113_Sowmya Chinthakindi/number_greater_10.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number: "))
+
+if a > 10:
+    print(f"The number greater than 10 is {a}")
