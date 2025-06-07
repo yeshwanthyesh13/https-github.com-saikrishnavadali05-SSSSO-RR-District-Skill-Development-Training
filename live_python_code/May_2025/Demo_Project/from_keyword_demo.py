31afd94f097a410b5fea4cafadfd7bf0bb407a0d
saikrishnavadali05 <saikrishnavadali05@gmail.com>
2025-06-07 19:53:28 +0530

    Added today class files

---
+++ live_python_code/May_2025/Demo_Project/from_keyword_demo.py
@@ -0,0 +1,9 @@
+from math_functions import addition
+from math_function_copy import addition as add, addition_copy
+a = 10
+b = 5
+print(addition(a, b))
+print(add(a, b))
+print(addition_copy(a, b))
+
+import my_utils
---
+++ live_python_code/May_2025/Demo_Project/using_modules_demo.py
@@ -0,0 +1,12 @@
+import my_utils
+import math_functions
+
+my_utils.greet("Alice")
+
+a = 10
+b = 5
+
+print(math_functions.addition(a, b))
+print(math_functions.subtraction(a, b))
+print(math_functions.multiplication(a, b))
+print(math_functions.division(a, b))
